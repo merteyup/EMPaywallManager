a798a5387322b4f5a6d625d9a92de80f8359b959
merteyup <eupmrt@gmail.com>
2025-05-12 18:58:04 +0300

    Add `PaywallViewProtocol`

--- Sources/EMPaywallManager/PaywallViewProtocol.swift
+++ Sources/EMPaywallManager/PaywallViewProtocol.swift
@@ -1,6 +1,15 @@
+//
+//  PaywallViewProtocol.swift
+//  EMPaywallManager
+//
+//  Created by Eyüp Mert on 8.05.2025.
+//
+
+import SwiftUI
+
 
 public protocol PaywallViewProtocol: View {
-    var onSubscribe: (() -> Void)? { get set }
+    var onSubscribe: ((Feature) -> Void)? { get set }
     var onRestore: (() -> Void)? { get set }
     var onDismiss: (() -> Void)? { get set }
 }
