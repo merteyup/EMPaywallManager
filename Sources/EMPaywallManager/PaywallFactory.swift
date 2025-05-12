fbd6f334f16e0921e9969eab1a46268b39e72d5f
merteyup <eupmrt@gmail.com>
2025-05-12 19:01:09 +0300

    Add `PaywallFactory` to make paywall creation process easier.

--- Sources/EMPaywallManager/PaywallFactory.swift
+++ Sources/EMPaywallManager/PaywallFactory.swift
@@ -1,26 +1,54 @@
+//
+//  PaywallFactory.swift
+//  EMPaywallManager
+//
+//  Created by Eyüp Mert on 8.05.2025.
+//
+
+import SwiftUI
+
+public enum PaywallType: String, CaseIterable {
+    case classic, modern, minimalist, bold, imageHeavy, animationDriven, testimonialBased, gradientStyle, fullScreen, splitLayout
+}
+
+
 public struct PaywallFactory {
+
+    @MainActor
     public static func make(
-        type: PaywallType,
-        model: PaywallModel,
-        onSubscribe: (() -> Void)? = nil,
-        onRestore: (() -> Void)? = nil,
-        onDismiss: (() -> Void)? = nil
-    ) -> some PaywallViewProtocol {
+           model: PaywallModel,
+           onSubscribe: ((Feature) -> Void)? = nil,
+           onRestore: (() -> Void)? = nil,
+           onDismiss: (() -> Void)? = nil
+    ) -> AnyView {
+        let type = PaywallABManager.shared.assignedType
+        let view: any PaywallViewProtocol
+        
         switch type {
         case .classic:
-            return PaywallView(
+            view = ClassicPaywallView(
                 model: model,
                 onSubscribe: onSubscribe,
                 onRestore: onRestore,
                 onDismiss: onDismiss
             )
         case .modern:
-            return ModernPaywallView( // this is your second design
+            view = ModernPaywallView(
                 model: model,
                 onSubscribe: onSubscribe,
                 onRestore: onRestore,
                 onDismiss: onDismiss
             )
+        case .minimalist:
+            view = MinimalistPaywallView(
+                model: model,
+                onSubscribe: onSubscribe,
+                onRestore: onRestore,
+                onDismiss: onDismiss
+            )
+        default:
+            fatalError("\(type.rawValue.capitalized) Paywall is not implemented yet.")
         }
+        return AnyView(view)
     }
 }
