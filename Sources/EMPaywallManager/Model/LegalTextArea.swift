d71c1bb97fa5872289325b4376b5d89303af5047
merteyup <eupmrt@gmail.com>
2025-05-14 10:57:41 +0300

    Add `LegalTextArea` into project

--- Sources/EMPaywallManager/Model/LegalTextArea.swift
+++ Sources/EMPaywallManager/Model/LegalTextArea.swift
@@ -1,5 +1,12 @@
+//
+//  LegalTextArea.swift
+//  EMPaywallManager
+//
+//  Created by Eyüp Mert on 13.05.2025.
+//
 
-    public struct LegalTextArea: Hashable {
-        let legalText: String
-        let legalLinks: [LegalLink]?
-    }
+
+public struct LegalTextArea: Hashable {
+    let legalText: String
+    let legalLinks: [LegalLink]?
+}
