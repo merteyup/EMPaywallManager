a03ed7c43871f5a276b5a3d00c39340a2e9f7bb4
merteyup <eupmrt@gmail.com>
2025-05-15 10:59:41 +0300

    Remove `PaywallCoordinator` and keep view showing logic simpler in `EMPaywallView` as like a router..

---
+++ Sources/EMPaywallManager/EMPaywallView.swift
@@ -0,0 +1,90 @@
+//
+//  EMPaywallView.swift
+//  EMPaywallManager
+//
+//  Created by Eyüp Mert on 8.05.2025.
+//
+
+import SwiftUI
+
+public enum PaywallType: String, CaseIterable {
+    case classic, modern, minimalist, bold, imageHeavy, animationDriven, testimonialBased, gradientStyle, fullScreen, splitLayout
+}
+
+public struct EMPaywallView: View {
+    let model: PaywallModel
+    let variant: PaywallType
+    let onSubscribe: ((Feature) -> Void)?
+    let onRestore: (() -> Void)?
+    let onDismiss: (() -> Void)?
+
+    public init(
+        model: PaywallModel,
+        variant: PaywallType,
+        onSubscribe: ((Feature) -> Void)? = nil,
+        onRestore: (() -> Void)? = nil,
+        onDismiss: (() -> Void)? = nil
+    ) {
+        self.model = model
+        self.variant = variant
+        self.onSubscribe = onSubscribe
+        self.onRestore = onRestore
+        self.onDismiss = onDismiss
+    }
+
+    public var body: some View {
+        switch variant {
+        case .bold:
+            BoldPaywallView(viewModel: BoldPaywallViewModel(
+                model: model,
+                onSubscribe: onSubscribe,
+                onRestore: onRestore,
+                onDismiss: onDismiss
+            ))
+
+        case .classic:
+            ClassicPaywallView(viewModel: ClassicPaywallViewModel(
+                model: model,
+                onSubscribe: onSubscribe,
+                onRestore: onRestore,
+                onDismiss: onDismiss
+            ))
+
+        case .minimalist:
+            MinimalistPaywallView(viewModel: MinimalistPaywallViewModel(
+                model: model,
+                onSubscribe: onSubscribe,
+                onRestore: onRestore,
+                onDismiss: onDismiss
+            ))
+
+        case .modern:
+            ModernPaywallView(viewModel: ModernPaywallViewModel(
+                model: model,
+                onSubscribe: onSubscribe,
+                onRestore: onRestore,
+                onDismiss: onDismiss
+            ))
+
+        default:
+            fatalError("Paywall type \(variant.rawValue) not implemented.")
+        }
+    }
+}
+
+public extension EMPaywallView {
+    static func withAssignedVariant(
+        using model: PaywallModel,
+        onSubscribe: ((Feature) -> Void)? = nil,
+        onRestore: (() -> Void)? = nil,
+        onDismiss: (() -> Void)? = nil
+    ) -> some View {
+        EMPaywallView(
+            model: model,
+            variant: PaywallABManager.shared.assignedType,
+            onSubscribe: onSubscribe,
+            onRestore: onRestore,
+            onDismiss: onDismiss
+        )
+    }
+}
